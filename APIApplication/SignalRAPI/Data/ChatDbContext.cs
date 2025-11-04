3effbcde167e99d72163bcc9460d9850ed6c1901
MeetShah <shahmeet199722@gmail.com>
2025-11-04 22:57:30 +0530

    DbContext added

---
+++ APIApplication/SignalRAPI/Data/ChatDbContext.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SignalRAPI.Data
+{
+    public class ChatDbContext(DbContextOptions<ChatDbContext> options) : DbContext(options)
+    {
+        public DbSet<ChatMessageEntity> ChatMessages => Set<ChatMessageEntity>();
+        protected override void OnModelCreating(ModelBuilder b)
+        {
+            b.Entity<ChatMessageEntity>(e =>
+            {
+                e.ToTable("ChatMessages");
+                e.HasKey(x => x.Id);
+                e.Property(x => x.User).HasMaxLength(64).IsRequired();
+                e.Property(x => x.Text).HasMaxLength(2000).IsRequired();
+                e.Property(x => x.Room).HasMaxLength(64);
+                e.HasIndex(x => new { x.Room, x.At });
+            });
+        }
+    }
+}
---
+++ APIApplication/SignalRAPI/Data/ChatMessageEntity.cs
@@ -0,0 +1,11 @@
+﻿namespace SignalRAPI.Data
+{
+    public class ChatMessageEntity
+    {
+        public int Id { get; set; }
+        public string User { get; set; } = default!;
+        public string Text { get; set; } = default!;
+        public DateTimeOffset At { get; set; }
+        public string? Room { get; set; } // null => global
+    }
+}
